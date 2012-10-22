d74e93eeadd69ac6d9c99da629d4e8f6b58e94df
Jimmy Lu <jimmy.lu.2011@gmail.com>
2012-10-22 11:22:00 -0400

    Added time to logs, changed slightly how logs are formatted, also changed how main() are included

--- test/_include/test_Log.h
+++ test/_include/test_Log.h
@@ -3,20 +3,22 @@
 
 #include <sstream>
 
-#if defined(__GCC__) || (__GNUC__) || (__MINGW32__)
+#if defined(__GNUC__) || \
+    defined(__MINGW32__) || \
+    defined(__MINGW64__)
  #define FUNC __PRETTY_FUNCTION__
 #else
  #define FUNC __func__
 #endif
 
 #define __log_print TestLog().get()
-#define __enter TestLog().get() << "<--Enter-->    " << FUNC
+#define __enter TestLog().get()            << "<--Enter-->         " << FUNC
 #define __return(parm) do {\
-                       TestLog().get() << "<--Leave-->    " << FUNC;\
+                       TestLog().get()     << "<--Leave-->         " << FUNC;\
                        return (parm);\
                        } while (0)
 #define __checkpoint(parm) do {\
-                           TestLog().get() << "CheckPoint: " << (parm);\
+                           TestLog().get() << "<--CheckPoint-->    " << (parm);\
                            } while (0)
 
 class TestLog {
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,12 +1,35 @@
+#ifdef _DEBUG
+
+// Dependencies
+#include <ctime>
 #include "_include/test_Base.h"
 #include "_include/test_List.h"
+#include "_include/test_Log.h"
 #include "_include/test_Run.h"
 
-#ifdef _DEBUG
+static void curTime ();
+
 int main ()
 {
+    curTime();
     initTest();
     runTest();
     cleanTest();
+    curTime();
+    __log_print << "\n";
 }
+
+void curTime ()
+{
+    std::time_t t = time(0);
+    std::tm *now = localtime(&t);
+    __log_print << "<--Current Time-->            "
+                << (now->tm_year + 1900) << '-'
+                << (now->tm_mon + 1)     << '-'
+                << (now->tm_mday)        << "    "
+                << (now->tm_hour)        << ':'
+                << (now->tm_min)         << ':'
+                << (now->tm_sec);
+}
+
 #endif
