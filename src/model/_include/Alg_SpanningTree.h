47909c6d67b96b11c5619574f0f0c93d571a7536
Jimmy Lu <jimmy.lu.2011@gmail.com>
2012-12-06 21:27:06 -0500

    In the progress of implementing spanning tree algorithm

--- src/model/_include/Alg_SpanningTree.h
+++ src/model/_include/Alg_SpanningTree.h
@@ -2,14 +2,32 @@
 #define ALG_SPANNING_TREE_H
 
 // Dependencies
+#include <vector>
 #include "AlgorithmBase.h"
+#include "ModelTypes.h"
+
+class Tree {
+public:
+    class Node {
+        Identity v_id;
+        std::vector<Node*> neighbours_;
+    };
+
+private:
+};
 
 class SpanningTree : public Algorithm {
 public:
     SpanningTree (LogicController &logic);
     virtual Result run (const Parameter& param) override;
 
+    Tree findSpanningTree (Identity g_id);
+
 private:
+    void findSpanningTree (Identity v_id,
+                           Identity parent_id,
+                           NodeStatus *node,
+                           Tree *tree);
 };
 
 #endif//ALG_SPANNING_TREE_H
--- src/model/algorithm/Alg_SpanningTree.cpp
+++ src/model/algorithm/Alg_SpanningTree.cpp
@@ -4,7 +4,8 @@
 #include <algorithm>
 #include <utility>
 #include "../_include/Graph.h"
-#include "../_include/ModelTypes.h"
+
+class SpanningTreeFound {};
 
 SpanningTree::SpanningTree (LogicController &logic)
 : Algorithm(logic)
@@ -14,6 +15,50 @@ SpanningTree::SpanningTree (LogicController &logic)
 SpanningTree::Result
 SpanningTree::run (const SpanningTree::Parameter& parm)
 {
-    Result result = { .hasPath_ = false };
-    return result;
+    throw "Should not be here";
+    return Result();
+}
+
+// Temporary approach!
+// Once the common Result/Param facility is done,
+// this code needs to be updated.
+Tree
+SpanningTree::findSpanningTree (Identity g_id)
+{
+    IdentityList list = logic_.verticesOf(g_id);
+    NodeStatus node;
+    Tree tree;
+    try {
+        findSpanningTree(*list.begin(), NULL_IDENTITY, &node, &tree);
+    }
+    catch (SpanningTreeFound&) {
+        // Tree should be populated.
+        return tree;
+    }
+
+    // Should not reach here.
+    return Tree();
+}
+
+void
+SpanningTree::findSpanningTree (Identity v_id,
+                                Identity parent_id,
+                                NodeStatus *node,
+                                Tree *tree)
+{
+    if (node->statusOf(v_id) == NodeStatus::GREY) {
+        // Reached a cycle.
+        return;
+    }
+
+    node->visit(v_id);
+    IdentityList neighbours = logic_.neighboursOf(v_id);
+    std::for_each(neighbours.begin(), neighbours.end(),
+                  [&] (const Identity id) {
+                      if (id != parent_id &&
+                          node->statusOf(id) != NodeStatus::BLACK) {
+                      }
+                  });
+
+    node->leave(v_id);
 }
